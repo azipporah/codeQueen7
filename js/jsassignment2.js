1f9ab6c467d3f8c57e9b74a89318ecc85789cb7c
azipporah <zipporahatusasiire@gmail.com>
2022-10-19 19:56:41 -0700

    adding jsAssignments

--- js/jsassignment2.js
+++ js/jsassignment2.js
@@ -1,23 +1,23 @@
 let names = ["Blair", "Shanon", "Zipporah", "David", "Brian"];
+// Kindly uncomment the code below the  UPPERCASED COMMENTS!! to run the respective code.
 
 // FOR EACH LOOP
 // used when you want to execute the same code for very element of the array
-// break; and continue; cannot be used in a for each function, if you try you get an error
-// names.forEach(function(name){
 
+// names.forEach(function(name){
 // if (name === "Zipporah") {
 //     console.log(name + " is my name.");
 //     return;
-//     // the return; key word just skips to the next iteration, works like continue;
+//     // the return; key word just skips to the next iteration, works like continue;     
+//     // break; and continue; cannot be used in a for each function, if you try you get an error
 // }
 // console.log(name);
 // })
 
 
-// FOR OF LOOP
-// for of loop with continue;
-// for(let name of names){
+// FOR OF LOOP:continue;
 
+// for(let name of names){
 //     if (name === "Zipporah") {
 //         console.log(name + " is my name.");
 //         continue;
@@ -25,9 +25,9 @@ let names = ["Blair", "Shanon", "Zipporah", "David", "Brian"];
 //     console.log(name);
 // }
 
-// for of loop with break;
-// for(let name of names){
+// FOR OF LOOP:break;
 
+// for(let name of names){
 //     if (name === "Zipporah") {
 //         console.log(name + " is my name.");
 //         break;
@@ -36,7 +36,24 @@ let names = ["Blair", "Shanon", "Zipporah", "David", "Brian"];
 // }
 
 
-// DO WHILE LOOP
+// DO WHILE LOOP: continue;
+
+// let n = 0;
+// do {
+//     console.log(names[n]);
+//     n++;
+//     if (names[n] === "Zipporah") {
+//         console.log(names[n] + " is my name.");
+//         continue;
+//         // continue; doesn't seem to work cz "Zipporah" in the array is still printed!
+//     }
+    
+// } while (n < names.length);
+
+
+
+// DO WHILE LOOP: break;
+
 // let n = 0;
 // do {
 //     console.log(names[n]);
@@ -44,8 +61,6 @@ let names = ["Blair", "Shanon", "Zipporah", "David", "Brian"];
 //     if (names[n] === "Zipporah") {
 //         console.log(names[n] + " is my name.");
 //         break;
-//         // continue;
-//         // continue; doesn't seem to work cz "Zipporah" is printed!
 //     }
     
 // } while (n < names.length);
